53c4f39c60c6dc874064a74f313f8c0f98036ac4
Mykola Vashchuk <mmykolavashchuk@gmail.com>
2026-05-21 13:01:22 +0300

    pract 1 to 3

---
+++ web-practicals/lab_3/emoji.js
@@ -0,0 +1,76 @@
+const dictionary = {
+  "сонце":    "☀️",
+  "хмара":    "☁️",
+  "дощ":      "🌧️",
+  "сніг":     "❄️",
+  "блискавка":"⚡",
+  "серце":    "❤️",
+  "усмішка":  "😊",
+  "сум":      "😢",
+  "сміх":     "😂",
+  "кава":     "☕",
+  "піца":     "🍕",
+  "яблуко":   "🍎",
+  "собака":   "🐶",
+  "кіт":      "🐱",
+  "книга":    "📚",
+  "телефон":  "📱",
+  "комп":     "💻",
+};
+a = [1, 2, 3]
+b = [[4, 5], [6, 7]]
+
+const dictionaryMap = new Map(Object.entries(dictionary))
+
+function translate(text) {
+  let result = text;
+  for (const word in dictionary) {
+    result = result.replaceAll(word, dictionary[word]);
+    let capitalized = word.charAt(0).toUpperCase() + word.slice(1);
+    result = result.replaceAll(capitalized, dictionary[word]);
+    let uppercase = word.toUpperCase();
+    result = result.replaceAll(uppercase, dictionary[word]);
+  }
+  return result;
+}
+
+function translateMap(text) {
+  return Array.from(dictionaryMap.entries()).reduce((result, [word, emoji]) => {
+    const capitalized = word.charAt(0).toUpperCase() + word.slice(1)
+    const uppercase = word.toUpperCase()
+
+    return result
+      .replaceAll(word, emoji)
+      .replaceAll(capitalized, emoji)
+      .replaceAll(uppercase, emoji)
+  }, text)
+}
+
+
+const combinedWithSpread = [...a, ...b]
+const nestedArray = [...a, b]
+
+
+console.log(combinedWithSpread)
+console.log(nestedArray)
+
+console.log(translateMap("сьогодні сонце і усмішка"))
+console.log(translateMap("СОНЦЕ і Хмара. Дощ?"))
+
+
+function translateReverse(text) {
+  let result = text;
+  for (const word in dictionary) {
+    const emoji = dictionary[word];
+    result = result.replaceAll(emoji, word);
+  }
+  return result;
+}
+
+console.log(translate("сьогодні сонце і усмішка"));
+console.log(translate("СОНЦЕ і Хмара. Дощ?"));
+console.log(translateReverse("☀️ і ☁️"));
+
+console.log(translateMap("сьогодні сонце і усмішка"));
+console.log(translateMap("СОНЦЕ і Хмара. Дощ?"));
+console.log(translateReverse("☀️ і ☁️"));
